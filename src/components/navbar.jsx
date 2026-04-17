87325c0a42558755998398400b192198805bf31f
harsh-suman5 <harshsuman457@gmail.com>
2026-04-17 15:33:34 +0530

    talwind css added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <>
-    <div className="navbar">
+    <div className="navbar text-4xl font-bold bg-black ">
         <div>
             {/* <img src="" alt="" /> */}
-          <h1>Shubham's Portfolio</h1>
+          <h1 className='text-4xl font-bold bg-linear-to-r from-pink-500 to-red-500 bg-clip-text text-transparent'>Shubham's Portfolio</h1>
         </div>
         <div >
           <ul id='navlist'>
-            <li><a href="/">home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="/projects">Projects</a></li>
             <li><a href="/login">Login</a></li>
           </ul>
