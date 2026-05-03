1b1bb16dd4620b748f2a6d0333b0b9910227c85b
harsh-suman5 <harshsuman457@gmail.com>
2026-05-04 00:11:48 +0530

    sidebar added to dashboard

--- src/components/dashboard/dash_home.jsx
+++ src/components/dashboard/dash_home.jsx
@@ -6,9 +6,20 @@ const Dash_home = () => {
     return (
         <>
             <Dash_navbar />
-            <div>
-                <h1>hello {username}</h1>
-      </div>
+            <div className="container">
+              <div className="sidebar">
+                 <ul>
+                <li><a href="/dash_home">Dashboard</a></li>
+                <li><a href="/dash_home">Courses</a></li>
+                <li><a href="/dash_home">Payment info</a></li>
+                <li><a href="/dash_home">Profile</a></li>
+                <li><a href="/dash_home">Logout</a></li>
+              </ul>
+              </div>
+              <div className="main">
+                <h1>hello {username}</h1> 
+              </div>
+            </div>
 
     </>
   )
