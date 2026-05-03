12aa458466aa2abd094d3132ee0bca8e34746cf7
harsh-suman5 <harshsuman457@gmail.com>
2026-05-03 17:05:24 +0530

    login register disabled for already login

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -28,8 +28,13 @@ const Footer = () => {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/projects">Projects</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/register">Register</a></li>
+            {!isLoggedIn && (
+              <>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
+               </>
+            )
+          }
           </ul>
         </div>
 
