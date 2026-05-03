96b4f326d74c96ff8859f18de7450ef604684f92
harsh-suman5 <harshsuman457@gmail.com>
2026-05-03 17:06:59 +0530

    removed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -28,13 +28,8 @@ const Footer = () => {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/projects">Projects</a></li>
-            {!isLoggedIn && (
-              <>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
-               </>
-            )
-          }
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Register</a></li>
           </ul>
         </div>
 
